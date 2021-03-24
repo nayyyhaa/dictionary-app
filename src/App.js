4df0bb40d58cd18d1705f28b5f5b668a5e535426
nayyyhaa <nehaguptang.ng@gmail.com>
2021-03-24 18:03:27 +0530

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./styles.css";
+import "./App.css";
 import { useState } from "react";
+import logo from "./logo.svg";
 const meaningDict = {
   tranquility: "state of being calm",
   neha: "eyes"
 };
+const words = Object.keys(meaningDict);
 export default function App() {
   const [word, setWord] = useState("");
 
@@ -17,12 +19,36 @@ export default function App() {
       setMeaning("no idea bro!");
     }
   }
+  function wordClickHandler(word) {
+    setMeaning(meaningDict[word]);
+  }
   return (
     <div className="App">
-      <h1>Dictionary</h1>
-      <input onChange={findMeaning} value={word} />
-      <h2>{word}</h2>
-      <h2>{meaning}</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Dictionary</h1>
+        <input
+          onChange={findMeaning}
+          value={word}
+          placeholder="unlock the mystery"
+        />
+        <h2>{word}</h2>
+        <h2>{meaning}</h2>
+        <h2>
+          Words I know:
+          {words.map((w) => (
+            <span
+              onClick={() => wordClickHandler(w)}
+              style={{
+                cursor: "pointer",
+                padding: "0.5rem"
+              }}
+            >
+              {w}
+            </span>
+          ))}
+        </h2>
+        </header>
     </div>
   );
 }
